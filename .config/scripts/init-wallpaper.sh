90b412706916b8c15d214c88c9d8c8caa562f93d
William Dreyer <w.h.dreyer@icloud.com>
2024-12-02 18:23:24 -0500

    cleaning up misc things and adding labels

--- .config/scripts/init-wallpaper.sh
+++ .config/scripts/init-wallpaper.sh
@@ -8,21 +8,15 @@ if [[ -f "$WALLPAPER_CACHE" ]]; then
         swww img "$WALLPAPER_PATH"
         wal -i "$WALLPAPER_PATH"
         if [[ "$WALLPAPER_PATH" =~ \.jpg$|\.jpeg$ ]]; then
-  	# It's a JPG file, so just copy it
 	  cp "$WALLPAPER_PATH" ~/.cache/cached-wallpaper.jpg
 	  echo "Wallpaper copied as JPG."
 	elif [[ "$WALLPAPER_PATH" =~ \.png$ ]]; then
-  	# It's a PNG file, so convert it to JPG
   	  convert "$WALLPAPER_PATH" ~/.cache/cached-wallpaper.jpg
   	  echo "Wallpaper converted from PNG to JPG."
 	else
-  	# Not a JPG or PNG file
   	  echo "Error: Unsupported wallpaper format. Only PNG and JPG are supported."
   	  exit 1
 fi
-
-
-
         echo "Wallpaper set to $WALLPAPER_PATH and pywal colors applied."
     else
         echo "Error: Wallpaper file '$WALLPAPER_PATH' does not exist."
--- .config/scripts/wallpaper.sh
+++ .config/scripts/wallpaper.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 WALLPAPER_DIR="$HOME/.config/wallpapers"
 
 WALLPAPER_PATH=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
@@ -12,19 +11,15 @@ swww img "$WALLPAPER_PATH"
 source "$HOME/.cache/wal/colors.sh"
 
 if [[ "$WALLPAPER_PATH" =~ \.jpg$|\.jpeg$ ]]; then
-  # It's a JPG file, so just copy it
   cp "$WALLPAPER_PATH" ~/.cache/cached-wallpaper.jpg
   echo "Wallpaper copied as JPG."
 elif [[ "$WALLPAPER_PATH" =~ \.png$ ]]; then
-  # It's a PNG file, so convert it to JPG
   convert "$WALLPAPER_PATH" ~/.cache/cached-wallpaper.jpg
   echo "Wallpaper converted from PNG to JPG."
 else
-  # Not a JPG or PNG file
   echo "Error: Unsupported wallpaper format. Only PNG and JPG are supported."
   exit 1
 fi
-
 
 echo "Wallpaper set to: $WALLPAPER_PATH"
 
