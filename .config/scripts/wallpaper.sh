964a35f9638459c0245538758b6e59cf2ad564b8
William Dreyer <w.h.dreyer@icloud.com>
2024-12-01 20:37:55 -0500

    swww script with pywal

---
+++ .config/scripts/wallpaper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+WALLPAPER_DIR="$HOME/.config/wallpapers"
+
+WALLPAPER_PATH=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
+wal -i "$WALLPAPER_PATH"
+
+swww img "$WALLPAPER_PATH"
+
+source "$HOME/.cache/wal/colors.sh"
+
+echo "Wallpaper set to: $WALLPAPER_PATH"
