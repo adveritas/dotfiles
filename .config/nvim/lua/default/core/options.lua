cbc6d5cbc7947799414cc44947b5791c4b10b2ea
William Dreyer <w.h.dreyer@icloud.com>
2024-12-02 20:26:36 -0500

    nvim init commit

---
+++ .config/nvim/lua/default/core/options.lua
@@ -0,0 +1,38 @@
+local opt = vim.opt
+
+-- numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- miscellaneous
+opt.wrap = false
+opt.cursorline = true
+
+-- appearance 
+
+
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+
+
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
+
