4ec0f55b383348f11aa35e9c3ae5bdaa5e2a61ee
William Dreyer <w.h.dreyer@icloud.com>
2024-12-03 18:19:54 -0500

    init commit nvim config

--- .config/nvim/lua/default/lazy.lua
+++ .config/nvim/lua/default/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("default.plugins")
+require("lazy").setup("default.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
---
+++ .config/nvim/lua/default/plugins/lualine.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+    -- configure lualine with modified theme
+    lualine.setup({
+      options = {
+        theme = "pywal16-nvim",
+      },
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff5c00" },
+          },
+        },
+      },
+    })
+  end,
+}
